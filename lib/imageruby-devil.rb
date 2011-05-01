ddbe1f0e5026e5b840245a1346f560dd867233fe
Dario <darios3@gmail.com>
2011-05-01 15:14:34 -0300

    created main header of the gem

---
+++ lib/imageruby-devil.rb
@@ -0,0 +1,22 @@
+=begin
+
+This file is part of the imageruby-devil project, http://github.com/tario/imageruby-devil
+
+Copyright (c) 2010 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+imageruby-devil is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+imageruby-devil is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with imageruby-devil.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
+require "rubygems"
+require "devil"

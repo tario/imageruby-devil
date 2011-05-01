92efe1a928ba7391083be9111ba1124446cb9733
Dario <darios3@gmail.com>
2011-05-01 16:49:47 -0300

    implemented general decoder using devil library

--- lib/devil_encoder/devil_encoder.rb
+++ lib/devil_encoder/devil_encoder.rb
@@ -19,13 +19,31 @@ along with imageruby-devil.  if not, see <http://www.gnu.org/licenses/>.
 
 =end
 require "devil"
+require "tempfile"
 
 require "imageruby/decoder"
 
 module ImageRuby
   class DevilDecoder < ImageRuby::Decoder
     def decode(data, image_class)
+      # creates a temp file with data
+      tmpfile = Tempfile.new("img")
+      tmpfile.write data
 
+      path = tmpfile.path
 
+      tmpfile.close
+
+      tmpfile2 = Tempfile.new("img2")
+
+      path2 = tmpfile2.path + ".bmp"
+      tmpfile2.close
+
+      Devil.with_image(path) do |img|
+        img.save(path2)
+      end
+
+      image_class.from_file(path2)
     end
+  end
 end

9af5ed57ab13afbfeffe6b881950c278abfcb5d0
Dario <darios3@gmail.com>
2011-05-14 17:11:02 -0300

    updated examples on README

--- examples/example1.rb
+++ examples/example1.rb
@@ -3,7 +3,7 @@ require "imageruby"
 
 include ImageRuby
 
-# create image (red gradient)
+# create image (red gradient and random green)
 image = Image.new(255,255) {|x,y| Color.from_rgb(x,rand(200),0) }
 
 # save image on png format
